c80cf1db8e8c73981b2b17930e54d6aafbb8f139
Joel Taylor <jtayped@gmail.com>
2023-05-08 21:28:54 +0200

    lists of articles created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { SideBar, MobileHeader, HomeMain } from "../containers";
 import withAuthentication from "../js/withAuthRedirect";
 import { auth } from "../config/firebase";
 
 const Home = () => {
+  const [mostLikedArticles, setMostLikedArticles] = useState([]);
+  const [recentArticles, setRecentArticles] = useState([]);
+
+  const [trendingArticles, setTrendingArticles] = useState([]);
+  const [discoveryArticles, setDiscoveryArticles] = useState([]);
+  const [followingArticles, setFollowingArticles] = useState([]);
+
   return (
     <>
       {auth.currentUser ? (
         <div className="overflow-hidden w-full h-screen relative md:flex z-0 text-slate-100">
-          {" "}
           <div className="hidden md:flex">
-            <SideBar />
+            <SideBar
+              recentArticles={recentArticles}
+              mostLikedArticles={mostLikedArticles}
+            />
           </div>
           <div className="flex md:hidden w-screen h-10">
             <MobileHeader />
           </div>
-          <HomeMain />
+          <HomeMain
+            trendingArticles={trendingArticles}
+            discoveryArticles={discoveryArticles}
+            followingArticles={followingArticles}
+          />
         </div>
       ) : null}
     </>
