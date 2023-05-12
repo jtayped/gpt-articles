55c42b91025fd61c6a9d27ab70e40165ef5c29c6
Joel Taylor <jtayped@gmail.com>
2023-05-12 21:34:13 +0200

    uid not detected fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 // React Util
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // JSX Elements
 import { Home, Auth, LogIn, SignUp } from "./pages";
 
+// Firebase
+import { auth } from "./config/firebase";
+import { LoadingMessage } from "./components";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(() => {
+      setIsLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <LoadingMessage message="Loading" centered={true} />
+      </div>
+    );
+  }
+
   return (
     <BrowserRouter>
       <Routes>
