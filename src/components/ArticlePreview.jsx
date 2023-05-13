4c6f60c7b6434a2eb73a4011371a212aa34b8d9e
Joel Taylor <jtayped@gmail.com>
2023-05-13 18:41:44 +0200

    article preview animation

--- src/components/ArticlePreview.jsx
+++ src/components/ArticlePreview.jsx
@@ -8,7 +8,7 @@ import Badge from "./Badge";
 const ArticlePreview = ({ article }) => {
   const maxTags = 2;
   return (
-    <li className="bg-gpt-100 h-[60px] rounded-lg shadow-sm">
+    <li className="bg-gpt-100 h-[60px] rounded-lg shadow-sm transition-transform duration-100 hover:scale-[102%]">
       <Link to="/linktoarticle" className="flex items-center gap-2 h-full">
         <img
           src="https://images.unsplash.com/photo-1682687218147-9806132dc697?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80"
