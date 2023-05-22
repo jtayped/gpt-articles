aaeda99af4c3d38aefce6b1d00ce54a4cac6a684
jtayped <87146558+jtayped@users.noreply.github.com>
2023-05-22 10:47:30 +0200

    ask user data created

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,3 +3,4 @@ export { default as MobileHeader } from "./MobileHeader";
 export { default as UserInfo } from "./UserInfo";
 export { default as ArticleGroup } from "./ArticleGroup";
 export { default as Search } from "./Search";
+export { default as AskUserData } from "./AskUserData";
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,12 +10,15 @@ import {
 import Logo from "../assets/vector/default-monochrome-black.svg";
 import Google from "../assets/vector/google.svg";
 import { createUser } from "../js/firebaseFunctions";
+import { AskUserData } from "../containers/AskUserData";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
+
+  const [hasCreated, setHasCreated] = useState(false);
   const navigate = useNavigate();
 
   const handleSignUp = async (e) => {
@@ -63,7 +66,7 @@ const SignIn = () => {
 
   const handleGoogleSignIn = async () => {
     try {
-      signInWithPopup(auth, googleProvider)
+      signInWithPopup(auth, googleProvider);
       navigate("/");
     } catch (err) {
       setError("There has been an error, try again later!");
@@ -77,76 +80,84 @@ const SignIn = () => {
         <img src={Logo} className="h-10 min-w-[100px]" alt="Logo" />
       </header>
       <main className="max-w-[350px] sm:w-[350px] text-center">
-        <header className="p-6">
-          <h1 className="text-3xl font-bold">Create your account!</h1>
-          <p>
-            <b>NOTE:</b> you are <b>not</b> creating an account for Chat GPT
-          </p>
-        </header>
-        <form
-          onSubmit={(event) => handleSignUp(event)}
-          className="flex flex-col gap-2"
-        >
-          <input
-            type="email"
-            name="email"
-            placeholder="Email address"
-            className={`px-4 py-3 border rounded-[3px] placeholder:text-gray-500 placeholder:text-lg text-lg outline-none focus:border-[#10A37F] transition-all duration-150 ${
-              error && error.includes("email")
-                ? "border-red-500"
-                : "border-gray-500/50"
-            }`}
-            autoComplete="email"
-            value={email}
-            onChange={handleInputChange}
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className={`px-4 py-3 border rounded-[3px] placeholder:text-gray-500 placeholder:text-lg text-lg outline-none focus:border-[#10A37F] transition-all duration-150 ${
-              error && error.includes("Password")
-                ? "border-red-500"
-                : "border-gray-500/50"
-            }`}
-            autoComplete="new-password"
-            value={password}
-            onChange={handleInputChange}
-          />
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm Password"
-            className={`px-4 py-3 border rounded-[3px] placeholder:text-gray-500 placeholder:text-lg text-lg outline-none focus:border-[#10A37F] transition-all duration-150 ${
-              error && error.includes("Password")
-                ? "border-red-500"
-                : "border-gray-500/50"
-            }`}
-            autoComplete="new-password"
-            value={confirmPassword}
-            onChange={handleInputChange}
-          />
-          {error && <p className="text-red-500 text-sm">{error}</p>}
-          <button
-            className="bg-[#10A37F] hover:bg-[#1A7F64] py-3 mt-3 rounded-[3px] transition-all duration-150 text-white text-lg"
-            type="submit"
-          >
-            Sign Up
-          </button>
-        </form>
-        <p className="my-3">
-          Already have an account?{" "}
-          <Link to="/login" className="text-[#26b38d]">
-            Log In
-          </Link>
-        </p>
-        <p className="mb-3 text-sm">OR</p>
-        <button
-          className="px-4 py-3.5 flex items-center gap-4 text-lg border w-full bg-white border-gray-500/40 rounded-[3px] hover:bg-gray-500/20 transition-all duration-200"
-          onClick={handleGoogleSignIn}
-        >
-          <img src={Google} className="h-6" alt="google" /> Continue with Google
-        </button>
+        {hasCreated ? (
+          <AskUserData />
+        ) : (
+          <>
+            {" "}
+            <header className="p-6">
+              <h1 className="text-3xl font-bold">Create your account!</h1>
+              <p>
+                <b>NOTE:</b> you are <b>not</b> creating an account for Chat GPT
+              </p>
+            </header>
+            <form
+              onSubmit={(event) => handleSignUp(event)}
+              className="flex flex-col gap-2"
+            >
+              <input
+                type="email"
+                name="email"
+                placeholder="Email address"
+                className={`px-4 py-3 border rounded-[3px] placeholder:text-gray-500 placeholder:text-lg text-lg outline-none focus:border-[#10A37F] transition-all duration-150 ${
+                  error && error.includes("email")
+                    ? "border-red-500"
+                    : "border-gray-500/50"
+                }`}
+                autoComplete="email"
+                value={email}
+                onChange={handleInputChange}
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className={`px-4 py-3 border rounded-[3px] placeholder:text-gray-500 placeholder:text-lg text-lg outline-none focus:border-[#10A37F] transition-all duration-150 ${
+                  error && error.includes("Password")
+                    ? "border-red-500"
+                    : "border-gray-500/50"
+                }`}
+                autoComplete="new-password"
+                value={password}
+                onChange={handleInputChange}
+              />
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                className={`px-4 py-3 border rounded-[3px] placeholder:text-gray-500 placeholder:text-lg text-lg outline-none focus:border-[#10A37F] transition-all duration-150 ${
+                  error && error.includes("Password")
+                    ? "border-red-500"
+                    : "border-gray-500/50"
+                }`}
+                autoComplete="new-password"
+                value={confirmPassword}
+                onChange={handleInputChange}
+              />
+              {error && <p className="text-red-500 text-sm">{error}</p>}
+              <button
+                className="bg-[#10A37F] hover:bg-[#1A7F64] py-3 mt-3 rounded-[3px] transition-all duration-150 text-white text-lg"
+                type="submit"
+              >
+                Sign Up
+              </button>
+            </form>
+            <p className="my-3">
+              Already have an account?{" "}
+              <Link to="/login" className="text-[#26b38d]">
+                Log In
+              </Link>
+            </p>
+            <p className="mb-3 text-sm">OR</p>
+            <button
+              className="px-4 py-3.5 flex items-center gap-4 text-lg border w-full bg-white border-gray-500/40 rounded-[3px] hover:bg-gray-500/20 transition-all duration-200"
+              onClick={handleGoogleSignIn}
+            >
+              <img src={Google} className="h-6" alt="google" /> Continue with
+              Google
+            </button>
+          </>
+        )}
       </main>
       <p className="mb-7 text-sm text-gray-600 text-center px-4">
         Remember! This is not the official Chat GPT website!
