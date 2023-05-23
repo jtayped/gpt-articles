db5b92cfa7a0c9f4c940406c8fc460ae31cda924
jtayped <87146558+jtayped@users.noreply.github.com>
2023-05-23 16:42:59 +0200

    profile picture dragging

--- src/containers/AskUserData.jsx
+++ src/containers/AskUserData.jsx
@@ -5,6 +5,9 @@ import React, { useState, useEffect } from "react";
 import DefaultPFP from "../assets/defaultPFP.webp";
 
 const AskUserData = () => {
+  const [draggedFile, setDraggedFile] = useState(null);
+  const [dragOver, setDragOver] = useState(false);
+
   const [username, setUsername] = useState("");
   const [error, setError] = useState("");
 
@@ -18,27 +21,53 @@ const AskUserData = () => {
     };
   }, []);
 
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    setDraggedFile(file);
+    console.log(file);
+  };
+
+  const handleDragEnter = (event) => {
+    event.preventDefault();
+    setDragOver(true);
+  };
+
+  const handleDragLeave = (event) => {
+    event.preventDefault();
+    setDragOver(false);
+  };
+
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col items-center">
       <form
-        className="flex flex-col gap-5 items-center mt-[-50px]"
+        className="flex flex-col gap-3 items-center max-w-[350px] sm:w-[350px]"
         action={(e) => handleSubmit(e)}
       >
-        <div className="w-[400px] h-[205px] shadow-lg rounded-lg border-[#10A37F95] border-2 border-dashed flex flex-col justify-center">
-          <h1 className="text-xl font-bold">Upload Profile Picture</h1>
-          <p className="text-sm">
-            Click{" "}
-            <span className="text-[#10A37F] decoration-[#10A37F] cursor-pointer underline-1">
-              here
-            </span>{" "}
-            to upload manually.
-          </p>
+        <div
+          className="h-[205px] w-full shadow-lg rounded-lg border-[#10A37F95] border-2 border-dashed flex flex-col justify-center"
+          onDrop={handleDrop}
+          onDragLeave={handleDragLeave}
+          onDragEnter={handleDragEnter}
+        >
+          {dragOver ? null : (
+            <>
+              <h1 className="text-xl font-bold">Upload Profile Picture</h1>
+              <p className="text-sm">
+                Click{" "}
+                <span className="text-[#10A37F] decoration-[#10A37F] cursor-pointer underline-1">
+                  here
+                </span>{" "}
+                to upload manually.
+              </p>
+            </>
+          )}
         </div>
         <input
           type="text"
           name="username"
           placeholder="Username"
-          className={`px-4 py-3 border rounded-[3px] placeholder:text-gray-500 placeholder:text-lg text-lg outline-none focus:border-[#10A37F] transition-all duration-150 ${
+          className={`px-4 py-3 border w-full rounded-[3px] placeholder:text-gray-500 placeholder:text-lg text-lg outline-none focus:border-[#10A37F] transition-all duration-150 ${
             error && error.includes("taken")
               ? "border-red-500"
               : "border-gray-500/50"
@@ -47,6 +76,12 @@ const AskUserData = () => {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
+        <button
+          className="bg-[#10A37F] hover:bg-[#1A7F64] w-full py-3 rounded-[3px] transition-all duration-150 text-white text-lg"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
