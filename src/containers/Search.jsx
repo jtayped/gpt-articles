5e1c050710e3fe726d708ec76d71c8e99cf9acc1
Joel Taylor <jtayped@gmail.com>
2023-06-02 11:53:45 +0200

    spacing error on mobile

--- src/containers/Search.jsx
+++ src/containers/Search.jsx
@@ -13,7 +13,7 @@ const Search = () => {
   }
 
   return (
-    <div className="absolute bottom-0 left-0 w-full min-h-[150px] md:min-h-fit border-t md:border-t-0 border-white/20 md:border-transparent md:bg-vert-light-gradient bg-gpt-400 md:!bg-transparent md:bg-vert-dark-gradient pt-2">
+    <div className="absolute bottom-0 left-0 w-full min-h-fit md:min-h-[150px] border-t md:border-t-0 border-white/20 md:border-transparent md:bg-vert-light-gradient bg-gpt-400 md:!bg-transparent md:bg-vert-dark-gradient pt-2">
       <form
         onSubmit={() => searchForArticles()}
         className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl"
